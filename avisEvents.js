7137e62c28d92620e1de5eb62b4b0d3144e3942f
Lezz Giles <lezzgiles@gmail.com>
2025-07-01 10:42:50 -0400

    Adding updates to goldsmith page, changing EcoTeam time, removing passed upcoming events

--- avisEvents.js
+++ avisEvents.js
@@ -242,8 +242,8 @@ var avisEvents = [
 		title: 'Eco Team Invasive Plant identification and Removal Session',
 		location: 'TBD',
 		description: 'Email Lynn at <a href="mailto:HamletofLynn@gmail.com">HamletofLynn@gmail.com</a> to be put on the Eco Team email list. You’ll get up-to-date notifications about the location.',
-		start: '2025-07-06T13:00',
-		end: '2025-07-06T15:30',
+		start: '2025-07-06T08:30',
+		end: '2025-07-06T11:00',
 	},
 
 	{
